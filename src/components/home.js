106e2bc8c184cc3bf839440c641d7d55dcf2dabf
Cezekwe1 <eaezekwem@gmail.com>
2019-05-22 15:05:26 -0400

    initial commit

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class home extends Component {
+  render() {
+    return (
+      <div>
+        <button>Login</button>
+        <button>SignUp</button>
+      </div>
+    );
+  }
+}
